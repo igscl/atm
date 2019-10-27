e2ff07868d7dbd41da688978ea947a9c72decf79
Ignacio Giadach <igiadach@gmail.com>
2019-10-28 10:15:45 +1100

    eureka - line for loop in print

--- printer.rb
+++ printer.rb
@@ -1,21 +1,43 @@
 # Printer
 
 printer = []
+printerf = []
+trash = []
 puts "Hi, input 5 items to print on your personalised frame"
 while printer.length <=4
     printer.push(gets.chomp.to_s)
 
 end
 
-puts "
-***************************
-* #{printer[0].concat(" " * -(printer.at(0).length - 20))}         *
-* #{printer[1].concat(" " * -(printer.at(1).length - 20))}         *
-* #{printer[2].concat(" " * -(printer.at(2).length - 20))}         *
-* #{printer[3].concat(" " * -(printer.at(3).length - 20))}         *
-* #{printer[4].concat(" " * -(printer.at(4).length - 20))}         *
-***************************
-"
+for i in printer
+    if i.class == String
+        printerf.push(i)
+    elsif i.class == Integer
+        trash.push(i)
+    end
+end
+
+puts "\n\n***************************"
+printerf.each { |x| puts ("*   " << (x.concat (" " * -(x.length - 20)) << "      *")) }
+puts "***************************\n\n"
+
+
+
+
+
+
+
+
+
+# puts "
+# ***************************
+# * #{printer[0].concat(" " * -(printer.at(0).length - 20))}         *
+# * #{printer[1].concat(" " * -(printer.at(1).length - 20))}         *
+# * #{printer[2].concat(" " * -(printer.at(2).length - 20))}         *
+# * #{printer[3].concat(" " * -(printer.at(3).length - 20))}         *
+# * #{printer[4].concat(" " * -(printer.at(4).length - 20))}         *
+# ***************************
+# "
 
 
 # for i in printer
@@ -32,3 +54,4 @@ puts "
 
 
 
+# printerf.each { |x| puts x.concat("hello") }
